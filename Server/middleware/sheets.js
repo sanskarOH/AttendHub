7746e506c2e3a0cb8de76353e18d51c932983ac0
meyyappan055 <meyyappan055@gmail.com>
2024-06-20 23:45:56 +0530

    addedsheets update feature

--- Server/middleware/sheets.js
+++ Server/middleware/sheets.js
@@ -8,18 +8,38 @@ const updateSheet = async (spreadsheetId, range, values) => {
         const valueInputOption = 'RAW';
         const resource = { values };
 
+        const existingValues = await sheets.spreadsheets.values.get({
+            spreadsheetId,
+            range
+        });
+
+        const hasNewValues = false;
+        for (let i = 0; i<existingValues.length;i++){
+            if (existingValues[i] !== values[i]){
+                hasNewValues = true;
+                break;
+            }
+        }
+
         // Debugging logs
         console.log('Spreadsheet ID:', spreadsheetId);
         console.log('Range:', range);
         console.log('Values:', values);
 
-        const result = await sheets.spreadsheets.values.update({
-            spreadsheetId,
-            range,
-            valueInputOption,
-            resource
-        });
-        console.log('Cells updated:', result.data.updatedCells);
+        if(!hasNewValues){
+            console.log('no new values to update in sheets!')
+            resource;
+        } else{
+            const result = await sheets.spreadsheets.values.update({
+                spreadsheetId,
+                range,
+                valueInputOption,
+                resource
+            });
+            console.log('Cells updated:', result.data.updatedCells);
+        }
+
+    
     } catch (error) {
         console.error('Error updating sheet:', error);
     }
